6ceb1054388517a834f232e63973f29cb4505826
Nathan Moos <moosingin3space@gmail.com>
2015-04-24 19:41:16 -0400

    added commenting to string library

--- stringptr.h
+++ stringptr.h
@@ -1,12 +1,19 @@
 #pragma once
 
-#include <string.h>
-
+// Defines a string datatype
+// Conveniently includes both a buffer and a length.
 typedef struct {
     char* buf;
     int len;
 } stringptr_t;
 
+// Dynamically allocates a string
 stringptr_t* stringptr_alloc(int len);
+
+// Concatenates two strings together
+// Returns the number of characters copied
 int stringptr_concat(stringptr_t* out, stringptr_t* a, stringptr_t* b);
+
+// De-allocates a dynamic string
 void stringptr_dealloc(stringptr_t* str);
+
