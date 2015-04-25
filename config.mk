49084cf8a1d6a7344dab80151f613c2b46c65bc5
Nathan Moos <moosingin3space@gmail.com>
2015-04-25 15:41:22 -0400

    removed zmq dependency

--- config.mk
+++ config.mk
@@ -7,15 +7,10 @@ LIBBSD_CFLAGS=$(shell pkg-config --cflags libbsd-overlay)
 LIBBSD_DIR=
 LIBBSD_LIB=$(shell pkg-config --libs libbsd-overlay)
 
-# zeromq, czmq
-ZEROMQ_CFLAGS=
-ZEROMQ_DIR=
-ZEROMQ_LIB=-lzmq -lczmq
-
 # All libraries
-LIB_CFLAGS=$(LIBBSD_CFLAGS) $(ZEROMQ_CFLAGS)
-LIBDIRS=$(LIBBSD_DIR) $(ZEROMQ_DIR)
-LIBS=$(LIBBSD_LIB) $(ZEROMQ_LIB) -lm
+LIB_CFLAGS=$(LIBBSD_CFLAGS) 
+LIBDIRS=$(LIBBSD_DIR) 
+LIBS=$(LIBBSD_LIB) -lm
 
 CFLAGS=-Wall -Werror $(LIB_CFLAGS)
 LDFLAGS=$(LIBS)
