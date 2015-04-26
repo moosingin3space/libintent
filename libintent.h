7c0734276818fda1b8ee441fada2ba41d3af898e
Nathan Moos <moosingin3space@gmail.com>
2015-04-26 15:14:47 -0400

    added basic intent interface

--- libintent.h
+++ libintent.h
@@ -5,12 +5,13 @@
 #include <string.h>
 #include <memory.h>
 
+// Defines a tuple for storing data
 typedef struct {
     int param_id;
-
     stringptr_t buffer;
 } tuple_t;
 
+// Defines an intent
 typedef struct {
     stringptr_t url;
 
@@ -18,4 +19,11 @@ typedef struct {
     tuple_t* params;
 } intent_t;
 
+// Defines a type for function pointers that respond to intents
+typedef void (*IntentHandler)(const intent_t intent);
 
+// Register an intent from a protocol
+int intent_register(const stringptr_t protocol, IntentHandler handler);
+
+// De-register an intent from a protocol
+int intent_deregister(const stringptr_t protocol);
