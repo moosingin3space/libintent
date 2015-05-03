94116541ddbcdf1bad260d0fc7b2f974116d7635
Nathan Moos <moosingin3space@gmail.com>
2015-05-03 12:00:05 -0400

    defined more API functions

--- libintent.h
+++ libintent.h
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <memory.h>
 
+// TODO investigate turning the communication objects into
+// Nanopb-compatible protocol buffers.
+
 // Defines a tuple for storing data
 typedef struct {
     int param_id;
@@ -18,11 +21,32 @@ typedef struct {
     tuple_t* params;
 } intent_t;
 
-// Defines a type for function pointers that respond to intents
+// Defines an application
+typedef struct {
+    char* short_name;
+    char* user_name;
+    char* version_string;
+} application_t;
+
+// Define a type for function pointers that validate intents
+typedef bool (*IntentValidator)(const intent_t intent);
+
+// Define a type for function pointers that respond to intents
 typedef void (*IntentHandler)(const intent_t intent);
 
-// Register an intent from a protocol
-int intent_register(const char* protocol, IntentHandler handler);
+// Register an intent at a protocol
+int intent_register(const char* protocol, 
+                    IntentValidator validator, 
+                    IntentHandler handler);
 
 // De-register an intent from a protocol
 int intent_deregister(const char* protocol);
+
+// List the intent handlers that can handle the given intent
+int intent_list(const intent_t intent, 
+                application_t* apps);
+
+// Submits an intent to a specific application
+int intent_submit(const intent_t intent, 
+                  const application_t app,
+                  IntentHandler response_handler);
